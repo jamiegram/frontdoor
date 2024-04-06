99eafde4eeb1ce92718d7a592503b8c5a0689973
jamiemcl001 <jamiemcl001@gmail.com>
2024-04-06 15:50:25 +0100

    infra: Don't duplicate the variables and provider configurations

--- infra/unprocessed-bucket.tf
+++ infra/unprocessed-bucket.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 5.44"
-    }
-  }
-
-  required_version = ">= 1.2.0"
-}
-
-variable "TFC_AWS_RUN_ROLE_ARN" {}
-variable "TFC_AWS_PROVIDER_AUTH" {}
-
-provider "aws" {
-  region = "eu-west-1"
-}
-
 resource "aws_s3_bucket" "unprocessed_image_bucket" {
   bucket = "jamiegram.org_unprocessed_bucket"
   tags = {
