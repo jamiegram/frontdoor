8dfb6d19203679c77efb0c63da137904af998a6e
jamiemcl001 <jamiemcl001@gmail.com>
2024-04-06 14:52:20 +0100

    build: use a MUTABLE setting for the ECR repository, as we want to mutate the :latest image

--- infra/registry.tf
+++ infra/registry.tf
@@ -18,7 +18,7 @@ provider "aws" {
 
 resource "aws_ecr_repository" "frontdoor_repository" {
   name = "frontdoor"
-  image_tag_mutability = "IMMUTABLE"
+  image_tag_mutability = "MUTABLE"
   tags = {
     service = "frontdoor"
     environment = "production"
