b1a8859b0c1ae0b6b4a7df560f1ce2f49155d2b2
jamiemcl001 <jamiemcl001@gmail.com>
2024-04-06 15:53:07 +0100

    infra: Rename the unprocessed bucket so that it doesn't fail creation

--- infra/unprocessed-bucket.tf
+++ infra/unprocessed-bucket.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "unprocessed_image_bucket" {
-  bucket = "jamiegram.org_unprocessed_bucket"
+  bucket = "jamiegram.org-unprocessed-bucket"
   tags = {
     service = "frontdoor"
     environment = "production"
